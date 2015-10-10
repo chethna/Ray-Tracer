26090c0772b40aaa2c06591c058d08c05110053b
Chethna Kabeerdoss <chethna@Macintosh-6.local>
2015-10-09 21:00:44 -0500

    Initial Commit

---
+++ Ray Tracer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Ray Tracer
+//
+//  Created by Chethna Kabeerdoss on 10/9/15.
+//  Copyright (c) 2015 Chethna Kabeerdoss. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
